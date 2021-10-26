9cb16c691f10d2fab6ec93447f82f5591bb3d386
John Do <jdo.dev45@gmail.com>
2021-10-26 11:50:25 -0700

    removed ejs, backend serves static files

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const path = require("path");
+// const bodyParser = require("body-parser")
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.static('public'))
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!')
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`)
 })
