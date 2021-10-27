181e876ba2230735f31bf3b73253a02bee3a6961
John Do <jdo.dev45@gmail.com>
2021-10-27 14:28:50 -0700

    switch to nodemon, using json file, can post tasks

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require("path");
-// const bodyParser = require("body-parser")
+const bodyParser = require("body-parser")
+const todo = require("./task.json")
 
+app.use(express.json())
 app.use(express.static('public'))
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+app.post('/addtask', (req, res) => {
+  let obj = req.body
+  obj.id = Date.now()
+  obj.checked = false
+  todo.tasks.push(obj)
+
+  console.log(todo.tasks)
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
