08c2dc2c8d0f1fcd03f4656f7e4e0f648e46feb9
John Do <jdo.dev45@gmail.com>
2021-11-03 11:16:15 -0700

    removed json, summited tasks appear on dom

--- index.js
+++ index.js
@@ -2,30 +2,24 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const path = require("path");
+const path = require("path")
 const bodyParser = require("body-parser")
-const todo = require("./task.json")
-const fs = require("fs")
 
 app.use(express.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const data = ['hello', 'world']
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 app.get('/getItems', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('task.json'))
   res.json(data)
 })
 
-app.post('/addtask', (req, res) => {
-  let obj = req.body
-  obj.id = Date.now()
-  obj.checked = false
-  todo.tasks.push(obj)
-  console.log(todo.tasks)
-  res.status(201)
+app.post('/addItem', (req, res) => {
+  data.push(req.body.text)
 })
 
 app.listen(process.env.PORT, () => {
--- public/main.js
+++ public/main.js
@@ -3,12 +3,39 @@ const input = document.getElementById("item")
 const list = document.getElementById("list")
 let items = []
 
+form.addEventListener('submit', addTask)
+
 window.onload = event => {
   fetch('/getItems')
     .then(res => res.json())
-    .then(res => {
-      items = res.tasks
-      console.log(items)
-    })
+    .then(res => items = res)
+    .then(showTasks)
     .catch(err => console.log(err))
 };
+
+function addTask(event) {
+  event.preventDefault()
+  fetch('/addItem', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({text: input.value})
+  })
+    .then(res => res.json())
+    .catch(err => console.log(err))
+  appendTask(input.value)
+  form.reset()
+}
+
+function showTasks() {
+  items.forEach(x => {
+    list.innerHTML += `<li>${x}</li>`
+  })
+}
+function appendTask(item) {
+  items.push(item)
+  const li = document.createElement('li')
+  li.innerText = item
+  list.append(li)
+}
